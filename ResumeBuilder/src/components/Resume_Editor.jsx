9f33f8e629480b178c46bde1f0cfa192b8cb5c46
SHUBHAM CHAUHAN <chshubh091@gmail.com>
2026-08-31 21:47:19 +0530

    ResumeForge

---
+++ ResumeBuilder/src/components/Resume_Editor.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+const Resume_Editor = ({ setName, setRole, setAbout }) => {
+    return (
+        <div>
+            <h2>Edit Your Resume</h2>
+
+            <label>Name</label>
+            <input type="text" placeholder="Enter your name" onChange={(e) => setName(e.target.value)} />
+
+            <label>Role</label>
+            <input type="text" placeholder="Enter your role" onChange={(e) => setRole(e.target.value)} />
+
+            <label>About</label>
+            <textarea placeholder="write something about yourself" onChange={(e) => setAbout(e.target.value)}></textarea>
+
+        </div>
+    )
+}
+
+export default Resume_Editor
