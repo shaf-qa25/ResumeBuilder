af1d05f9a54c24edc8b46d3693cc4d5ad0edf504
Gargi Yadav <gargiyadav711@gmail.com>
2026-09-01 00:11:04 +0530

    made some changes

--- src/components/Resume_Editor.jsx
+++ src/components/Resume_Editor.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-const Resume_Editor = ({ setName, setRole, setAbout }) => {
+const Resume_Editor = ({ setName, setRole, setAbout, setEmail}) => {
     return (
         <div>
             <h2>Edit Your Resume</h2>
@@ -11,6 +11,9 @@ const Resume_Editor = ({ setName, setRole, setAbout }) => {
 
             <label>Role</label>
             <input type="text" placeholder="Enter your role" onChange={(e) => setRole(e.target.value)} />
+
+            <label>Email ID</label>
+            <input type="email" placeholder="enter your college email id" onChange={(e) => setEmail(e.target.value)} />
 
             <label>About</label>
             <textarea placeholder="write something about yourself" onChange={(e) => setAbout(e.target.value)}></textarea>
