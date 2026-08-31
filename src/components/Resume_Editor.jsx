298da2a5daee6ee0991e76513e48f1277d21912f
Vaishnavi Jaiswal <vaishnavijais2609@gmail.com>
2026-08-31 20:29:31 +0530

    newchanges

--- src/components/Resume_Editor.jsx
+++ src/components/Resume_Editor.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 
 
-const Resume_Editor = ({ setName, setRole, setAbout }) => {
+const Resume_Editor = ({ setName, setRole, setHobbies, setAbout }) => {
     return (
         <div>
             <h2>Edit Your Resume</h2>
@@ -10,7 +10,14 @@ const Resume_Editor = ({ setName, setRole, setAbout }) => {
             <input type="text" placeholder="enter your name" onChange={(e) => setName(e.target.value)} />
 
             <label>Role</label>
-            <input type="text" placeholder="Enter your role" onChange={(e) => setRole(e.target.value)} />
+            <input type="text" placeholder="Enter your role" onChange={(e) => setRole(e.target.value)
+            } />
+
+           <div className="input-group">
+            <label>Hobbies</label>
+            <input type="text" placeholder="Enter your hobbies" onChange={(e) => setHobbies(e.target.value)}/>
+             </div>
+
 
             <label>About</label>
             <textarea placeholder="write something about yourself" onChange={(e) => setAbout(e.target.value)}></textarea>
--- src/components/Resume_Preview.jsx
+++ src/components/Resume_Preview.jsx
@@ -1,4 +1,4 @@
-const Resume_Preview = ({ name, role, about }) => {
+const Resume_Preview = ({ name, role, hobbies, about }) => {
     return (
         <div className="resume">
             <h2>Resume Preview</h2>
@@ -9,6 +9,10 @@ const Resume_Preview = ({ name, role, about }) => {
                     <h1>{name || "Your Name"}</h1>
                     <h3>{role || "Your Role"}</h3>
                 </div>
+                <div className="resume-section">
+                <h3>Hobbies</h3>
+              <p>{hobbies || "Your hobbies"}</p>
+               </div>
 
                 {/* Divider */}
                 <div className="resume-divider"></div>
