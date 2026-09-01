67cce9d254c10ac26fe1ef43797f2e530e3baa1a
Ambesh Kumar Sharma <ambeshkumarsharma246@gmail.com>
2026-09-01 15:39:46 +0530

    Changes i have done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from "react";
 import "./App.css";
 import Resume_Editor from "./components/Resume_Editor";
@@ -7,6 +8,10 @@ import StarBorder from "./components/StarBorder";
 function App() {
     const [name, setName] = useState("");
     const [role, setRole] = useState("");
+    const [studentno, setstudentno] = useState("");
+    const [collegemail, setcollegemail] = useState("");
+    const [society, setsociety] = useState("");
+    const [college, setcollege] = useState("");
     const [about, setAbout] = useState("");
 
     return (
@@ -14,18 +19,23 @@ function App() {
             <h1>Live Resume Editor</h1>
 
             <div className="resume-container">
+
                 <StarBorder
                     as="div"
                     className="editor-section"
-                    color="#0284c7"
+                    color="#1876a5"
                     speed="6s"
                     thickness={4}
                     backgroundColor="#ffffff"
-                    borderColor="rgba(14, 165, 233, 0.35)"
+                    borderColor="rgba(7, 95, 136, 0.35)"
                 >
                     <Resume_Editor
                         setName={setName}
+                        setstudentno={setstudentno}
+                        setcollegemail={setcollegemail}
+                        setsociety={setsociety}
                         setRole={setRole}
+                        setcollege={setcollege}
                         setAbout={setAbout}
                     />
                 </StarBorder>
@@ -33,18 +43,23 @@ function App() {
                 <StarBorder
                     as="div"
                     className="preview-section"
-                    color="#0284c7"
+                    color="#22a6e9"
                     speed="6s"
                     thickness={4}
                     backgroundColor="#ffffff"
-                    borderColor="rgba(14, 165, 233, 0.35)"
+                    borderColor="rgba(8, 114, 163, 0.35)"
                 >
                     <Resume_Preview
                         name={name}
                         role={role}
                         about={about}
+                        student={studentno}
+                        mail={collegemail}
+                        society={society}
+                        college={college}
                     />
                 </StarBorder>
+
             </div>
         </div>
     );
